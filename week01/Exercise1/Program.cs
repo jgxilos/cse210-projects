6dea741d1a3109a550c96d4a884d459e2ba88013
jgxilos <jernigam.gonzalez@gmail.com>
2025-07-03 13:57:34 -0400

    Task performed with comments

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,9 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask the user for his or her name.
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
+        //Ask the user for his or her last name.
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
