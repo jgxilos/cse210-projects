353bf430d72626a7775a295452f58a0bb8accb49
jgxilos <jernigam.gonzalez@gmail.com>
2025-07-08 18:16:49 -0400

    Task performed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create Job instances
+        Job job1 = new Job()
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job()
+        {
+            _jobTitle = "Manager",
+            _company = "Apple",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        // Create instance of Resume
+        Resume myResume = new Resume()
+        {
+            _name = "Allison Rose"
+        };
+
+        // Adding jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Show complete resume
+        myResume.Display();
     }
 }
